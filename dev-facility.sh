ff1cefd3d2381c17eff4aead20f6667be3c7c1a4
Jean Bordat <jbordat@ssense.com>
2018-12-19 13:35:54 -0500

    create development facilitator (inotify support)

---
+++ dev-facility.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/jbordat/Documents/Projects/gitflow
+
+echo "use it as root : 'sudo /bin/bash ./dev-facility'"
+
+# inotifywait -e close_write --format %e git-doflow* |
+while inotifywait -e close_write --format %e git-*flow*; do
+  make uninstall && make install
+done
