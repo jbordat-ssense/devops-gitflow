24ca5f7e1e8aaaa1fd657fc487ab1fc6612fc0fa
Jean Bordat <jbordat@ssense.com>
2018-12-19 13:49:42 -0500

    dev facility change

--- dev-facility.sh
+++ dev-facility.sh
@@ -5,6 +5,6 @@ cd /home/jbordat/Documents/Projects/gitflow
 echo "use it as root : 'sudo /bin/bash ./dev-facility'"
 
 # inotifywait -e close_write --format %e git-doflow* |
-while inotifywait -e close_write --format %e git-*flow*; do
+while inotifywait -e close_write --format %e git*; do
   make uninstall && make install
 done
